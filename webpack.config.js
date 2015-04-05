cdc8df23bb3766583055a176da4f2d48c8ab3330
Jason Wang <jason@genebygene.com>
2015-04-05 13:26:38 -0500

    initial boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  debug: true,
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    "./src/main.js",
+  ],
+  output: {
+    path: "./public/build/",
+    filename: "bundle.js",
+		publicPath: "/build/"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules|bower_components/, loaders: ["babel-loader"]},
+      { test: /\.jsx$/, exclude: /node_modules|bower_components/, loaders: ["react-hot", "babel-loader", "eslint-loader"]}
+    ]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    definePlugin
+  ]
+}
+
+
