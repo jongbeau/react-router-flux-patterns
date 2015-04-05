9f0a3171e19d62b3f109042d1c0b81c04d2892c3
Jason Wang <jason@genebygene.com>
2015-04-05 14:43:57 -0500

    route handling works

--- src/components/app.js.jsx
+++ src/components/app.js.jsx
@@ -1,9 +1,20 @@
 var React = require('react')
+var {Link, RouteHandler} = require('react-router')
 
 module.exports = React.createClass({
   render() {
     return (
-      <h1>React Router Flux Patterns!</h1>
+      <div>
+        <h1>React Router Flux Patterns!</h1>
+        <ul>
+          <li><Link to='items'>Item List</Link></li>
+          <li><Link to='item' params={{id: 1}}>Item</Link></li>
+          <li><Link to='new'>New Item</Link></li>
+        </ul>
+        <div>
+          <RouteHandler/>
+        </div>
+      </div>
     )
   }
 })
